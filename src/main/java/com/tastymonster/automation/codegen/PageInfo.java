bc83ab4826ca22427e4adf5e12ac0bf58db2976c
sukalpo.mitra <sukalpo.mitra>
2016-04-15 14:42:57 +0800

    Added support to read test data mapped with test id of a field. A developer can change field ids and names. But he should never change testid once defined. The framework should then read from a hasmap of testid and values passed and generate the test file too

--- src/main/java/com/tastymonster/automation/codegen/PageInfo.java
+++ src/main/java/com/tastymonster/automation/codegen/PageInfo.java
@@ -5,71 +5,79 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Each PageInfo object represents a single "page", as defined by the caller. If a page represents more than one template, it will be  
- * a combination of these PageInfo objects meshed together
+ * Each PageInfo object represents a single "page", as defined by the caller. If
+ * a page represents more than one template, it will be a combination of these
+ * PageInfo objects meshed together
+ * 
  * @author Marcus
- *
+ * 
  */
 public class PageInfo {
 
-	//This is only protected for unit testing purposes
-	protected Set<FieldDetails> fields = new HashSet<FieldDetails>();
-	protected String pageName;
-	protected String pageURI;
-	protected PresentationType presentationType;
-	
-	public Set<FieldDetails> getFields() {
-		return fields;
-	}
+    // This is only protected for unit testing purposes
+    protected Set<FieldDetails> fields = new HashSet<FieldDetails>();
+    protected String pageName;
+    protected String pageURI;
+    protected PresentationType presentationType;
 
-	public void setFields( Set<FieldDetails> fields ) {
-		this.fields = fields;
-	}
+    public Set<FieldDetails> getFields() {
+        return fields;
+    }
 
-	public String getPageName() {
-		return pageName;
-	}
+    public void setFields(Set<FieldDetails> fields) {
+        this.fields = fields;
+    }
 
-	public void setPageName(String pageName) {
-		this.pageName = pageName;
-	}
+    public String getPageName() {
+        return pageName;
+    }
 
-	public String getPackage() {
-		return "com.tastymonster.automation.page.base";
-	}
+    public void setPageName(String pageName) {
+        this.pageName = pageName;
+    }
 
-	public void setPageURI( String pageURI ) {
-		this.pageURI = pageURI;
-	}
+    public String getPackage() {
+        return "com.tastymonster.automation.page.base";
+    }
 
-	public String getPageURI() {
-		return pageURI;
-	}
+    public String getPageFieldPackage() {
+        return "com.tastymonster.automation.director.user";
+    }
 
-	public PresentationType getPresentationType() {
-		return presentationType;
-	}
+    public void setPageURI(String pageURI) {
+        this.pageURI = pageURI;
+    }
 
-	public void setPresentationType(PresentationType presentationType) {
-		this.presentationType = presentationType;
-	}
+    public String getPageURI() {
+        return pageURI;
+    }
 
-	/**
-	 * The file will be parsed according to its implementation. It will call parseVelocity, parseJSP, or 
-	 * whatever type (plug-in) is appropriate
-	 * @param file
-	 * @return
-	 */
-	public void initPage( File file ) {
-		//Get parser from factory by sending it the file
-		//The factory will return an IPresentationParser with the correct implementation
-		// and its file contents initialized
-		IPresentationParser parser = ParserFactory.getParser( file );
+    public PresentationType getPresentationType() {
+        return presentationType;
+    }
 
-		this.setPageName( parser.getPageName() );
-		this.setPageURI( parser.getPageURI() );
+    public void setPresentationType(PresentationType presentationType) {
+        this.presentationType = presentationType;
+    }
 
-		//Call the methods on the parser to populate the correct field details
-		this.setFields( parser.buildFieldDetails() );
-	}
+    /**
+     * The file will be parsed according to its implementation. It will call
+     * parseVelocity, parseJSP, or whatever type (plug-in) is appropriate
+     * 
+     * @param file
+     * @return
+     */
+    public void initPage(File file) {
+        // Get parser from factory by sending it the file
+        // The factory will return an IPresentationParser with the correct
+        // implementation
+        // and its file contents initialized
+        IPresentationParser parser = ParserFactory.getParser(file);
+
+        this.setPageName(parser.getPageName());
+        this.setPageURI(parser.getPageURI());
+
+        // Call the methods on the parser to populate the correct field details
+        this.setFields(parser.buildFieldDetails());
+    }
 }
